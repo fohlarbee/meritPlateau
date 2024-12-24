2d31e7c528d899a2eb5461f9e839ff4ea5033ee8
fohlarbee <sammyola246@gmail.com>
2024-12-24 06:33:52 +0100

    facet 1

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,23 +1,54 @@
+"use client"
+import Image from 'next/image'
 import React from 'react'
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false)
   return (
-    <div className=''>
-        <div className="flex flex-col lg:flex-row justify-center  lg:justify-between items-center gap-5">
-            <img src="/assets/logo.svg" alt="logo" />
-            <div className="flex flex-col lg:flex-row justify-center  lg:justify-between items-center gap-5">
-                <p>Courses</p>
-                <p>About Us</p>
-                <p>Teacher</p>
-                <p>Pricing</p>
-                <p>Careers</p>
-                <p>Blog</p>
-            </div>
-            <button className='btn bg-white text-[#7270dd] border-none hover:bg-[#7270dd] hover:text-white capitalize rounded-full'>
-                Get Started
+  <nav className='flex flex-row justify-between w-full items-center '>
+        <div className="">
+            <Image src="/assets/logo.svg" alt="logo"  width={50} height={50}/>
+        </div>
+        <div className="flex-row justify-center  lg:justify-between items-center gap-5 hidden lg:flex ">
+                <p>About us</p>
+                <p>Program</p>
+                <p>Events</p>
+                <p>Get Involved</p>
+                <p>Contact</p>
+                <p>Donate</p>
+        </div>
+        <div className="">
+            <button className='btn hidden md:flex bg-[#441752] text-[#fff] border-none hover:bg-[#6a3a7a] hover:text-white capitalize rounded-full'>
+                    Get Started
+            </button>
+            <button 
+            onClick={() => setIsMenuOpen(!isMenuOpen)}
+            className='btn flex md:hidden bg-[#eee] text-[#fff] border-none hover:bg-[#6a3a7a] hover:text-white capitalize rounded-full'>
+                    <Image src="/assets/menu.svg" alt="menu" className='text-[#441752]' width={20} height={20}/>
             </button>
         </div>
-    </div>
+        {isMenuOpen && (
+            <div className='flex flex-col gap-5 absolute top-16 right-0 text-center bg-[#fff5f1]  rounded-3xl w-full px-6 lg:px-16 py-10  '>
+              <button className='absolute top-2 right-5 bg-[#eee] py-2 px-2 rounded-full border-none hover:bg-[#fff]'>
+                <Image 
+                src="/assets/close.svg" 
+                alt="close" 
+                width={20} 
+                height={20} 
+                onClick={() => setIsMenuOpen(!isMenuOpen)}
+                className='text-[#441752]'
+                />
+              </button>
+                <p>About us</p>
+                <p>Program</p>
+                <p>Events</p>
+                <p>Get Involved</p>
+                <p>Contact</p>
+                <p>Donate</p>
+            </div>
+            
+        )}
+    </nav>
   )
 }
 
--- components/Values.js
+++ components/Values.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 const Values = () => {
@@ -11,7 +12,7 @@ const Values = () => {
       </p>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 lg:gap-16 py-20">
         <div className="flex flex-col gap-5 items-center">
-          <img src="/assets/v1.svg" alt="v1" />
+          <Image src="/assets/v1.svg" alt="v1" width={100} height={100} />
           <p className="text-2xl font-semibold">Speaking clubs</p>
           <p className="text-gray-500 text-center">
             In our school you will practice <br /> your speaking skills and just get <br /> a
@@ -19,14 +20,14 @@ const Values = () => {
           </p>
         </div>
         <div className="flex flex-col gap-5 items-center">
-          <img src="/assets/v2.svg" alt="v1" />
+          <Image src="/assets/v2.svg" alt="v1"  width={100} height={100} />
           <p className="text-2xl font-semibold">Quality control</p>
           <p className="text-gray-500 text-center">
             In our school you will practice <br /> your speaking skills and just get <br /> a
             lot of positive emotions!
           </p>
         </div> <div className="flex flex-col gap-5 items-center">
-          <img src="/assets/v3.svg" alt="v1" />
+          <Image src="/assets/v3.svg" alt="v1"  width={100} height={100} />
           <p className="text-2xl font-semibold">Progress analysis</p>
           <p className="text-gray-500 text-center">
             In our school you will practice <br /> your speaking skills and just get <br /> a
